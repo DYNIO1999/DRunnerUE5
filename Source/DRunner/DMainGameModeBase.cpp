8565a23df74c0bf705df7cb084e2de338434e8cf
Daniel Skrobot <dd7412506@gmail.com>
2023-12-28 13:16:57 +0100

    fixed code responsible for generating map

--- Source/DRunner/DMainGameModeBase.cpp
+++ Source/DRunner/DMainGameModeBase.cpp
@@ -37,40 +37,47 @@ void ADMainGameModeBase::StartPlay()
 	if (MyGameInstance)
 	{
 		
-		//UE_LOG(LogTemp, Warning, TEXT("Witdh %d"), MyGameInstance->ImageLevelInfo.ImageWidthSize);
-		//UE_LOG(LogTemp, Warning, TEXT("Height %d"), MyGameInstance->ImageLevelInfo.ImageHeightSize);
-
 		const int ImageWidth = MyGameInstance->ImageLevelInfo.ImageWidthSize;
-		const int ImageHeight = MyGameInstance->ImageLevelInfo.ImageHeightSize;
 		const int BytesPerPixel = MyGameInstance->ImageLevelInfo.PixelSizeInBytes;
 
 		const auto& ImageRawData = MyGameInstance->ImageLevelInfo.ImageData;
+		
 		int PixelCount = 0;
 		
 		float PlatformPosX = 0;
 		float PlatformPosY = 0;
 		float PlatformPosZ = 0;
-		
-		int count =0;
+
+		float PlatformConstantOffset = 1100;
+
 		for (size_t i=0;i<ImageRawData.Num();i++)
 		{
 			
-			
 			if(((i+1) % BytesPerPixel) ==0)
 			{
+				
+				
 				bool IsValid = true;
 				
+				if(PixelCount==ImageWidth)
+				{
+					PlatformPosX=0;
+					PlatformPosY+=PlatformConstantOffset;
+					PixelCount= 0;
+				}
+				
 				TArray<int> Pixel;
-				//UE_LOG(LogTemp,Error, TEXT("J %d"), i-BytesPerPixel);	
 				for(int j=i-(BytesPerPixel-1);j<i;j++)
 				{
 					
 					Pixel.Push(ImageRawData[j]);
 						
 				}
+				
 				for(size_t k=0;k<BytesPerPixel-1;k++)
 				{
-					if (Pixel[k] != 255)
+					//Choosing right platform
+					if (Pixel[k] == 255)
 					{
 						IsValid = false;
 					}
@@ -86,57 +93,16 @@ void ADMainGameModeBase::StartPlay()
 						SpawnParams.Owner = this;
 					
 						FRotator SpawnRotation = FRotator(0.0f, 0.0f, 0.0f);
-			
-						// Spawn the actor
+						
 						AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(ActorToSpawn, PlatformVectorPos, SpawnRotation, SpawnParams);
-			
-						UE_LOG(LogTemp,Error, TEXT("Spawned Actor %d"), PixelCount);
-						//UE_LOG(LogTemp, Warning, TEXT("Height %d"), MyGameInstance->ImageLevelInfo.ImageHeightSize);
-						// Additional code to interact with the spawned actor
+					//	UE_LOG(LogTemp,Error, TEXT("Spawned Actor %d"), PixelCount);
 					}
-					PlatformPosX+=1050;
 				}
-
-				count++;
+				
+				PlatformPosX+=PlatformConstantOffset;
+				PixelCount++;
 			}
-			
-			// if (((i % BytesPerPixel) == 0) || i !=0)
-			// {
-			// 	PixelCount++;
-			// 	
-			// 	
-			// 	
-			//
-			// 	PlatformPosX+=20;
-			// 	if (PixelCount > ImageWidth)
-			// 	{
-			// 		PlatformPosY+=20;
-			// 		PlatformPosX=0;
-			// 	}
-			//
-			// 	if (IsValid)
-			// 	{
-			// 		FVector PlatformVectorPos= FVector(PlatformPosX, PlatformPosY, PlatformPosZ);
-			//
-			// 		if (GetWorld())
-			// 		{
-			// 			FActorSpawnParameters SpawnParams;
-			// 			SpawnParams.Owner = this;
-			// 		
-			// 			FRotator SpawnRotation = FRotator(0.0f, 0.0f, 0.0f);
-			//
-			// 			// Spawn the actor
-			// 			AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(ActorToSpawn, PlatformVectorPos, SpawnRotation, SpawnParams);
-			//
-			// 			UE_LOG(LogTemp,Error, TEXT("Spawned Actor %d"), PixelCount);
-			// 			//UE_LOG(LogTemp, Warning, TEXT("Height %d"), MyGameInstance->ImageLevelInfo.ImageHeightSize);
-			// 			// Additional code to interact with the spawned actor
-			// 		}
-			// 	}
-			// 	
-			
 		}
-		UE_LOG(LogTemp,Error, TEXT("Pixel Count %d"), count);
 	}
 
 		
